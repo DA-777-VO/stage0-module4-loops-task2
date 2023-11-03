d67e265a5d2605e90dff25bdf39e511ab87ec7ec
David Apakin <apakin.david@gmail.com>
2023-11-03 20:27:50 +0400

    4.2.2 v3

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -3,13 +3,22 @@ package school.mjc.stage0.loops.task2;
 public class EvenNumbersPrinter {
     public void printEvenNumbers(int printTillInclusive) {
 
-        int counter = 0;
-        while (0 < printTillInclusive){
-            if (counter % 2 == 0) {
-                System.out.println(counter);
-            }
-            counter++;
+//        int counter = 0;
+//        while (0 < printTillInclusive){
+//            if (counter % 2 == 0) {
+//                System.out.println(counter);
+//            }
+//            counter++;
+//        }
+
+
+        int i = 0;
+        while (i <= printTillInclusive) {
+            System.out.println(i);
+            i += 2;
         }
+
+
 
     }
 }
